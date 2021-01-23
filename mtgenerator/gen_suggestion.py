532c1c325b0b563f242057f62fb8fb6bcb297ebe
Oliver Hare <oliver.parsons.hare@gmail.com>
2021-01-23 14:44:23 -0500

    improved deck suggester

--- mtgenerator/gen_suggestion.py
+++ mtgenerator/gen_suggestion.py
@@ -1,61 +1,120 @@
+import argparse
 import random as r
+import math as m
 
-SUG_NUM = 3
-WILD_WEIGHT = 0.05
-BAN_NUM = 2
-END_WEIGHT = 0.45
-NUM_PLAYERS = 2
+THEME_NUM = 1
+TRIBE_NUM = 1
+MECH_NUM = 1
+WILD_NUM = 3
+BAN_NUM = 5
+END_WEIGHT = 0.25
 
-colors = ["red", "black", "blue", "white", "green"]
-keywords = {
-    "red": ["knights", "goblins", "dragons", "instants", "satyrs", "warriors"],
-    "black": ["deathtouch", "flying", "sacrifice", "enchantments", "graveyard", "heal", "mill", "vampire", "discard", "rogues", "knights", "zombies", "demons", "nightmare"],
-    "blue": ["flying", "instants", "counter", "wizards", "rogues", "mill", "enchantments", "water", "scry"],
-    "white": ["cats", "clerics", "knights", "enchantments", "flying", "humans", "dogs", "heal", "angels"],
-    "green": ["counters", "landRamp", "spiders", "elves"],
-    "wildcard": ["artifact", "party", "landfall", "colorless", "mutate", "adventure"]
+COLORS = ["red", "black", "blue", "white", "green"]
+
+TRIBES = {
+    "red": ["knights", "goblins", "dragons", "satyrs", "warriors", "dwarves", "dogs", "firey"],
+    "black": ["vampire", "rogues", "knights", "zombies", "demons", "nightmare", "snakes", "swampy"],
+    "blue": ["wizards", "rogues", "artifacts", "birds", "merfolk", "sphinx", "watery"],
+    "white": ["cats", "clerics", "knights", "humans", "dogs", "angels", "soldiers", "warriors", "kor"],
+    "green": ["spiders", "elves", "dinosaurs", "elementals", "beasts", "animals", "insects", "plants", "serpents"],
 }
 
-for player in range(NUM_PLAYERS):
+MECHANICS = {
+    "red": ["instants", "discard", "sacrifice", "equipment", "burn", "landfall", "conversion"],
+    "black": ["deathtouch", "flying", "sacrifice", "enchantments", "graveyard", "heal", "mill", "discard", "removal"],
+    "blue": ["flying", "instants", "counter", "mill", "enchantments", "scry"],
+    "white": ["enchantments", "flying", "heal", "equipment", "landfall"],
+    "green": ["counters", "ramp", "landfall", "food"],
+    "wild": ["artifacts", "party", "mutate", "adventure", "sagas", "gods"]
+}
 
+def suggest_deck(themes=THEME_NUM, tribes=TRIBE_NUM, mechs=MECH_NUM, wildcards=WILD_NUM, bans=BAN_NUM, colors=None, color_weight=END_WEIGHT):
     # Choose Colors
-    player_colors = set()
-    picking_colors = True
-    while picking_colors:
-        cur_num = len(player_colors)
-        if cur_num == 0:
-            player_colors.add(r.choice(colors))
-        elif cur_num == 1:
-            player_colors.add(r.choice(colors))
-        elif r.random() > END_WEIGHT:
-            player_colors.add(r.choice(colors))
+    if colors is None:
+        colors = 1
+        while r.random() > (color_weight ** (1 / colors)) and colors < 5:
+            colors += 1
+    deck_colors = r.sample(COLORS, colors)
 
-        if cur_num == len(player_colors):
-            picking_colors = False
+    # Build Tribe/Mech Lists
+    rel_tribes = []
+    rel_mechs = []
+    rel_wild = MECHANICS["wild"]
+    for color in deck_colors:
+        rel_tribes += TRIBES[color]
+        rel_mechs += MECHANICS[color]
 
-    # Choose Keywords
-    player_suggestions = set()
-    while len(player_suggestions) < SUG_NUM:
-        player_suggestions.add(r.choice(keywords[r.choice(list(player_colors))]))
-    if r.random() < WILD_WEIGHT:
-        player_suggestions.add(r.choice(keywords["wildcard"]))
+    # Choose Themes
+    deck_themes = r.sample(rel_tribes + rel_mechs, themes)
+    for theme in deck_themes:
+        if theme in rel_tribes:
+            rel_tribes.remove(theme)
+        else:
+            rel_mechs.remove(theme)
 
-    # Choose Bans
-    num_themes = 0
-    for color in player_colors:
-        num_themes += len(keywords[color])
+    # Choose requirements
+    deck_tribes = r.sample(rel_tribes, tribes)
+    rel_tribes = [tribe for tribe in rel_tribes if tribe not in deck_tribes]
+    deck_mechs = r.sample(rel_mechs, mechs)
+    rel_mechs = [mech for mech in rel_mechs if mech not in deck_mechs]
+    deck_wildcards = r.sample(rel_tribes + rel_mechs + rel_wild, wildcards)
+    for wildcard in deck_wildcards:
+        if wildcard in rel_tribes:
+            rel_tribes.remove(wildcard)
+        elif wildcard in rel_mechs:
+            rel_mechs.remove(wildcard)
+        
+        if wildcard in rel_wild:
+            rel_wild.remove(wildcard)
 
-    player_bans = set()
-    while len(player_bans) < BAN_NUM:
-        if num_themes == len(player_bans) + len(player_suggestions):
-            break
+    deck_requirements = deck_tribes + deck_mechs + deck_wildcards
             
-        proposed_ban = r.choice(keywords[r.choice(list(player_colors))])
-        if proposed_ban not in player_suggestions:
-            player_bans.add(proposed_ban)
+    # Choose Bans
+    deck_bans = r.sample(rel_tribes + rel_mechs + rel_wild, bans)
 
-    print(f"Player {player + 1}")
-    print(f"Colors: {' '.join(player_colors)}")
-    print(f"Suggestions: {' '.join(player_suggestions)}")
-    print(f"Bans: {' '.join(player_bans)}")
-    print()
+    # Return deck Object
+    return {
+        "colors": deck_colors,
+        "themes": deck_themes,
+        "reqs": deck_requirements,
+        "bans": deck_bans 
+    }
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-themes", type=int,
+        help="Number of themes to be suggested")
+    parser.add_argument("-tribes", type=int,
+        help="Number of tribes to be suggested")
+    parser.add_argument("-mechs", type=int,
+        help="Number of mechanics to be suggested")
+    parser.add_argument("-wildcards", type=int,
+        help="Number of additional mechanics and themes to be suggested")
+    parser.add_argument("-bans", type=int,
+        help="Number of mechanics and themes to ban")
+    parser.add_argument("-colors", type=int,
+        help="Number of colors to be suggested")
+    parser.add_argument("-color_weight", type=float,
+        help="Probability weight used to randomly generate the number of colors for the deck")
+    args = parser.parse_args()
+
+    arg_dict = {
+        "themes": args.themes,
+        "tribes": args.tribes,
+        "mechs": args.mechs,
+        "wildcards": args.wildcards,
+        "bans": args.bans,
+        "colors": args.colors,
+        "color_weight": args.color_weight
+    }
+
+    deck = suggest_deck(**{k: v for k, v in arg_dict.items() if v is not None})
+
+    print("Deck Suggestion:")
+    print(f"Colors: {', '.join(deck['colors'])}")
+    print(f"Themes: {', '.join(deck['themes'])}")
+    print(f"Requirements: {', '.join(deck['reqs'])}")
+    print(f"Bans: {', '.join(deck['bans'])}")
+
+if __name__ == "__main__":
+    main()
