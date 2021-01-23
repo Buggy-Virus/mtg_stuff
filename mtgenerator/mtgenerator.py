4c344d5e48e2e561b35ddc457a11bc96d5190c17
Oliver Hare <oliver.parsons.hare@gmail.com>
2021-01-23 17:45:37 -0500

    cleaned up rolldeck code, now uses improved suggester

--- mtgenerator/mtgenerator.py
+++ mtgenerator/mtgenerator.py
@@ -2,55 +2,94 @@ import os
 import discord
 import random
 from dotenv import load_dotenv
-from gen_suggestion
+from gen_suggestion import suggest_deck
 
 # .env
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-
 client = discord.Client()
 
-# Function to roll a deck, with suggestions and bans, for a single player
 
-def rollDeck(user, colorblind=False):
+def rollDeck(user, args):
+    # Parse colorblind and deck suggestion args
+    colorblind = ("colorblind" in args)
 
-  # assemble return string
-  introLine = ":crossed_swords: \n" "Deck prompt for "+ user + "\n"
+    deck_args = [
+        "themes", 
+        "tribes", 
+        "mechs", 
+        "wildcards", 
+        "bans", 
+        "colors", 
+        "color_weight",
+    ]
 
-  colorLine = "Colors: "
-  for color in player_colors:
-    if colorblind:
-        colorLine += color + " "
-    else:
-        colorLine += color_emoticon[color] + "  "
-  colorLine += "\n"
+    kwargs = {}
+    for arg, value in args.items():
+        if arg in deck_args:
+            # assume the first value of any argument is correct
+            kwargs[arg] = value[0]
 
-  suggestionsLine = "Themes:  " + ' '.join(player_themes) + "\n"
-  banLine = "Bans:  " + ' '.join(player_bans) + "\n"
+    # Generate deck suggestion
+    deck = suggest_deck(**kwargs)
 
-  ret = introLine + colorLine + suggestionsLine + banLine
-  return(ret)
+    # Assemble return string
+    intro = ":crossed_swords: \n" "Deck prompt for "+ user + " :crossed_swords:\n"
+
+    colors = "Colors: "
+    for color in deck["colors"]:
+        if colorblind:
+            colors += f"{color} "
+        else:
+            colors += f"{color_emoticon[color]}  "
+    colors += "\n"
+
+    themes = f"Themes:  {' '.join(deck['themes'])}\n"
+    reqs = f"Requirements:  {' '.join(deck['reqs'])}\n"
+    bans = f"Bans:  {' '.join(deck['bans'])}\n"
+
+    return intro + colors + themes + reqs + bans
+
+
+def parse_command(message):
+    tokens = message.content.split()
+    command = tokens.pop(0)
+    args_dict = {}
+    cur_arg = None
+    for token in tokens:
+        if token[:2] == "--":
+            cur_arg = token[2:]
+            args_dict[cur_arg] = []
+        elif cur_arg is not None:
+            args_dict[cur_arg].append(token)
+        else:
+            arg_error = {f"malformed command, \"{message.content}\", expected first argument beginning with \"--\", got {token}"}
+            print(arg_error)
+            await message.channel.send(arg_error)
+    return command, args_dict
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    if message.content[0] != "!":
+        return
 
-    if message.content == '!rolldeck':
+    command, args = parse_command(message)
+
+    if command == '!rolldeck':
         user = str(message.author)
-        response = rollDeck(user)
+        response = rollDeck(user, args)
         print(response)
         await message.channel.send(response)
 
-    if message.content == '!rolldeck-colorblind':
-        user = str(message.author)
-        response = rollDeck(user, True)
-        print(response)
-        await message.channel.send(response)
 
 @client.event
 async def on_ready():
+    print(f'mtgenerator bot spinning up')
     for guild in client.guilds:
         if guild.name == GUILD:
             print(
@@ -59,4 +98,9 @@ async def on_ready():
             )
             break
 
-client.run(TOKEN)
+def main():
+    print("running mtgenerator discord bot")
+    client.run(TOKEN)
+
+if __name__ == "__main__":
+    main()
