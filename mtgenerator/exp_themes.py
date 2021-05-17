e96ebf810b34c4f5567dc9b4fc421eb588ebda63
Oliver Hare <oliver.parsons.hare@gmail.com>
2021-05-16 20:23:54 -0400

    strixhaven themes

--- mtgenerator/exp_themes.py
+++ mtgenerator/exp_themes.py
@@ -1,3 +1,21 @@
+STRIXHAVEN_TRIBES = {
+    "Red": ["Humans", "Shamans", "Dwarves", "Spirits"],
+    "Black": ["Warlocks", "Humans", "Eldritch", "Inklings", "Pests"],
+    "Blue": ["Wizards", "Humans", "Birds", "Books"],
+    "White": ["Humans", "Wizards", "Clerics", "Birds", "Kor", "Spirit", "Academia"],
+    "Green": ["Mammals", "Druids", "Elves", "Swampy", "Pests"],
+    "Wild": []
+}
+
+STRIXHAVEN_MECHANICS = {
+    "Red": ["Haste", "Burn", "Instants"],
+    "Black": ["Counters", "Heal", "Flying", "Discard", "Sacrifice", "Graveyard"],
+    "Blue": ["Flying", "Bounce", "Tap", "Instants", "Counter", "Draw"],
+    "White": ["Exile", "Counters", "Buff", "Aura", "Enchantments"],
+    "Green": ["Trample", "Ramp", "Heal", "Counters", "Sacrifice"],
+    "Wild": ["Learn", "Magecraft", "Artifacts"]
+}
+
 KALDHEIM_TRIBES = {
     "Red": ["Giants", "Berserkers", "Dwarves", "Dragons"],
     "Black": ["Berserkers", "Elves", "Angels", "Zombies", "Demons"],
@@ -28,13 +46,14 @@ EVERGREEN_TRIBES = {
 EVERGREEN_MECHANICS = {
     "Red": ["Instants", "Discard", "Sacrifice", "Equipment", "Burn", "Landfall", "Conversion"],
     "Black": ["Deathtouch", "Flying", "Sacrifice", "Enchantments", "Graveyard", "Heal", "Mill", "Discard", "Destroy"],
-    "Blue": ["Flying", "Instants", "Counter", "Mill", "Enchantments", "Scry", "Tap"],
+    "Blue": ["Flying", "Instants", "Counter", "Mill", "Enchantments", "Scry", "Tap", "Bounce", "Draw"],
     "White": ["Enchantments", "Flying", "Heal", "Equipment", "Landfall", "Exile"],
     "Green": ["Counters", "Ramp", "Landfall", "Food"],
     "Wild": ["Artifacts", "Party", "Mutate", "Adventure", "Sagas", "Gods"]
 }
 
 themes = {
+    "Strixhaven": {"tribes": STRIXHAVEN_TRIBES, "mechs": STRIXHAVE_MECHANICS},
     "Kaldheim": {"tribes": KALDHEIM_TRIBES, "mechs": KALDHEIM_MECHANICS},
     "Evergreen": {"tribes": EVERGREEN_TRIBES, "mechs": EVERGREEN_MECHANICS},
 }
